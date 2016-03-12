7ec127c53434604483ebdb57615c383525e1d64a
bangwu <bwu@thoughtworks.com>
2016-03-12 13:04:48 +0800

    init user repository

--- src/main/java/com/bang/controller/User.java
+++ src/main/java/com/bang/controller/User.java
@@ -4,6 +4,14 @@ public class User {
     private String name;
     private String password;
 
+    public User() {
+    }
+
+    public User(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
+
     public void setName(String name) {
         this.name = name;
     }
--- src/main/java/com/bang/respository/UserRepository.java
+++ src/main/java/com/bang/respository/UserRepository.java
@@ -1,9 +1,28 @@
 package com.bang.respository;
 
 import com.bang.controller.User;
+import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
 public class UserRepository {
+
+    private static List<User> users = new ArrayList<>();
+
+    static {
+        users.add(new User("name1", "password1"));
+        users.add(new User("name2", "password2"));
+        users.add(new User("name3", "password3"));
+    }
+
     public User getUserByName(String name) {
+        for(User user : users){
+            if( user.getName().equals(name)){
+                return user;
+            }
+        }
         return null;
     }
 }
